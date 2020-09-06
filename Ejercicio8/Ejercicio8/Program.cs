93b7d857a5ab84d7b75a0c18483123925762f84a
bertei <70592221+bertei@users.noreply.github.com>
2020-09-06 15:17:57 -0300

    c1

---
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    //Crear una aplicación que, cuando el usuario ingrese una fecha, calcule la diferencia entre
+    //ésta y el día de hoy, mostrando por pantalla "La diferencia de fechas es de *nnnnn* días."
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fechastring = "";
+            DateTime fecha_hoy = DateTime.Now; //variable que tira la fecha del dia de hoy dia/mes/anio
+            TimeSpan diferencia_fechas; //variable que representa un intervalo de tiempo
+            DateTime fechasalida;
+            int resultadodias;
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("Ingrese una fecha en formato (dia/mes/anio).");
+                fechastring = Console.ReadLine();
+                if (!DateTime.TryParse(fechastring, out fechasalida))
+                {
+                    Console.WriteLine("Ingrese una fecha correcta.");
+                }
+                else
+                {
+                    flag = true;
+                }
+
+            } while (flag == false);
+            //} while (DateTime.TryParse(fechastring, out fechasalida) != true); // ciclo se repite mientras lo que ingrese no sea una fecha valida, osea dif a true, pq si escribo bien es true y salgo del ciclo
+
+            diferencia_fechas = fecha_hoy - fechasalida;
+            resultadodias = diferencia_fechas.Days;
+
+
+            Console.WriteLine("La diferencia entre la fecha de hoy y la ingresada es de: {0}.", resultadodias);
+        }
+    }
+}
---
+++ Ejercicio9/Ejercicio9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    //Crear una aplicación que pida al usuario su nombre e indique por pantalla “¡Hola,
+    //* usuario*!” si ingresa su nombre, en caso contrario dirá “No te conozco”.
+    //Por ejemplo, si el usuario es Suyai, e ingresa el nombre “Suyai” dirá “¡Hola, Suyai!”, si no,
+    //“No te conozco”
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre_usuario = "";
+            int comprobacion_nombre;
+            bool flag = false;
+
+            do
+            {
+                Console.WriteLine("Ingrese su nombre: ");
+                nombre_usuario = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(nombre_usuario))
+                {
+                    Console.WriteLine("No te conozco.");
+                }
+                else if (int.TryParse(nombre_usuario, out comprobacion_nombre))
+                {
+                    Console.WriteLine("No te conozco.");
+                }
+                else
+                {
+                    Console.WriteLine("Hola, {0}.", nombre_usuario);
+                    flag = true;
+                }
+            } while (flag == false);
+        }
+    }
+}
