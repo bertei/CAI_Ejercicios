86cab635c9a933cbda069b74101a529471b8fab7
bertei <70592221+bertei@users.noreply.github.com>
2020-09-05 23:52:48 -0300

    c1

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    //Crear una aplicación que, cuando el usuario ingrese dos números, realice la operación
+    //aritmética(a+b)* (a-b) e indique por pantalla el resultado.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1, n2;
+            int salidanro1 = 0;
+            int salidanro2 = 0;
+            int operacion = 0;
+            bool flag;
+
+            do
+            {
+                Console.WriteLine("Ingrese el primer numero.");
+                n1 = Console.ReadLine();
+                flag = ValidarNro(n1, ref salidanro1);
+                Console.WriteLine("Ingrese el segundo numero.");
+                n2 = Console.ReadLine();
+                flag = ValidarNro(n2, ref salidanro2);
+            } while (flag == false);
+            operacion = (salidanro1 + salidanro2) * (salidanro1 - salidanro2);
+            Console.WriteLine("El resultado es de : " + operacion);
+        }
+
+        private static bool ValidarNro(string numero, ref int salidanro)
+        {
+            bool flag = false;
+            if (!int.TryParse(numero, out salidanro))
+            {
+                Console.WriteLine("Debe ingresar un dato numerico.");
+            }
+            else if (salidanro < 0)
+            {
+                Console.WriteLine("Debe ingresar un numero positivo.");
+            }
+            else
+            {
+                flag = true;
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    //Crear una aplicación que, cuando el usuario ingrese cinco números, indique cuál es mayor,
+    //cuál es valor intermedio y cuál el menor.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynros = new int[5];
+            int maximo = 0;
+            int minimo = 0;
+            int promedio = 0;
+            int sumatoria = 0;
+
+            for (int i = 0; i < arraynros.Length; i++)
+            {
+
+                Console.WriteLine("Ingrese el numero " + (i + 1) + " de la lista.");
+                arraynros[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    maximo = arraynros[0];
+                    minimo = arraynros[0];
+                }
+                if (arraynros[i] > maximo)
+                {
+                    maximo = arraynros[i];
+                }
+                if (arraynros[i] < minimo)
+                {
+                    minimo = arraynros[i];
+                }
+            }
+
+            for (int i = 0; i < arraynros.Length; i++)
+            {
+                sumatoria += arraynros[i];
+            }
+            promedio = (sumatoria / arraynros.Length);
+
+
+            Console.WriteLine("El numero maximo del array de nros es: {0}.", maximo);
+            Console.WriteLine("El numero minimo del array de nros es: {0}.", minimo);
+            Console.WriteLine("El promedio del array es: " + promedio);
+        }
+    }
+}
